ea894bf2922c2bdfcb3ae2b34efcdd314cf04078
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-05-24 18:13:46 +0100

    Dev: JWT Actions

--- Program.cs
+++ Program.cs
@@ -1,11 +1,69 @@
 using GymBro_Backend_API.Data;
 using GymBro_Backend_API.Mapping;
+using GymBro_Backend_API.Repositories.Implementations;
+using GymBro_Backend_API.Repositories.Interfaces;
+using GymBro_Backend_API.Services.Implementations;
+using GymBro_Backend_API.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//jwt authentication
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings.GetValue<string>("SecretKey");
+Debug.WriteLine(secretKey);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            Debug.WriteLine("AUTH FAILED: " + context.Exception.Message);
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            Debug.WriteLine("TOKEN VALIDATED");
+            return Task.CompletedTask;
+        },
+        OnChallenge = context =>
+        {
+            Debug.WriteLine("JWT CHALLENGE: " + context.ErrorDescription);
+            return Task.CompletedTask;
+        }
+    };
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+             // **READ FROM SAME PATH AS LOGIN**
+             builder.Configuration.GetSection("JwtSettings:SecretKey").Value
+         )),
+
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetSection("JwtSettings:Issuer").Value, // **READ FROM SAME PATH**
+
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("JwtSettings:Audience").Value, // **READ FROM SAME PATH**
+
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromMinutes(5)
+
+    };
+});
+
+
+// Add services to the container.
 builder.Services.AddDbContext<GymBroDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -13,6 +71,8 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddAutoMapper(typeof(UserProfile).Assembly);
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -22,8 +82,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-
 app.UseHttpsRedirection();
+
+app.UseAuthentication();  
 app.UseAuthorization();
 
 app.MapControllers();
